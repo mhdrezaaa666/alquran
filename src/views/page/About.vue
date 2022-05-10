4d631601d88812935e70f0d870d038b4a36b3a3d
MRivalKurniawan <12050116070@students.uin-suska.ac.id>
2022-05-10 16:22:43 +0700

    Initial commit

---
+++ src/views/page/About.vue
@@ -0,0 +1,67 @@
+<template>
+  <!-- Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light shadow-sm mb-3" style="background-color: #57f0d9">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="../components/Logo_Quran-removebg-preview.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+        Al-Qur'an Digital
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.home' }" class="nav-link" href="#Home">Home Qur'an</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.about' }" class="nav-link active" aria-current="page" href="#about">About Qur'an</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.contact' }" class="nav-link" href="#contact">Contact Author</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar-->
+
+  <!-- Jumbotron-->
+  <section class="jumbotron text-center mb-5  p-3" style="">
+    <img src="../components/Logo_Quran-removebg-preview.png" alt="Logo Quran" width="200" class="rounded-circle img-thumbnail" />
+    <h1 class="display-4">Al-Qur'an</h1>
+    <hr>
+  </section>
+  <!-- Akhir Jumbotron-->
+
+  <!-- About-->
+  <section id="about">
+    <div class="container">
+      <div class="row text-center mt-5">
+        <div class="col">
+          <h2 class="display-7">About Qur'an</h2>
+        </div>
+      </div>
+      <div class="col justify-content-center fs-5 text-center">
+        <div class=" fs-6">
+          <p>“Al-Qur'an ini di buat secara digital agar mempermudah audiens jika di saat tidak membawa Al-Qur'an, jadi audiens tinggal menggunakan Al-Qur'an digital berbasis website. Adapun maksud kenapa Al-Qur'an ini di buat atas pengalaman author sendiri yang mana merasakan di saat dalam perjalanan lupa membawa Al-Qur'an.”</p>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+  <!-- Akhir About-->
+
+
+
+  <!-- Footer -->
+  <footer class="text-dark text-center fixed-bottom " style="background-color: #57f0d9">
+    <p>Created by <a href="https://www.instagram.com/m.rival_kurniawan/" class="text-dark fw-bold">M. Rival Kurniawan</a></p>
+  </footer>
+  <!-- Akhir Footer -->
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/views/page/Thank.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- Navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light shadow-sm mb-3" style="background-color: #57f0d9">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="../components/Logo_Quran-removebg-preview.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+        Al-Qur'an Digital
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.home' }" class="nav-link" href="#Home">Home Qur'an</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.about' }" class="nav-link" href="#about">About Qur'an</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{ name: 'page.contact' }" class="nav-link active" aria-current="page" href="#contact">Contact Author</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Akhir Navbar-->
+
+  <!-- Foto-->
+  <div class="text-center">
+    <img src="../components/Logo_Quran-removebg-preview.png" class="rounded" alt="icon" width="350" />
+  </div>
+  <!-- Akhir Foto-->
+
+  <!-- About-->
+  <section id="about">
+    <div class="container">
+      <div class="row text-center mt-5">
+        <div class="col">
+          <h2 class="display-6">Terima Kasih</h2>
+        </div>
+      </div>
+      <div class="row justify-content-center fs-5 text-center">
+        <div class="col-md-4 fs-6">
+          <p>"Terima Kasih Kepada Audiens Yang Sudah Memberikan Pesan Atau Sarannya Kepada Author, Agar Bisa Menjadi Yang Lebih Baik Lagi Dan Semakin Semangat Dalam Menjalankan Berbagai Project."</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Akhir About-->
+
+  <!-- Footer -->
+  <footer class="text-dark text-center fixed-bottom " style="background-color: #57f0d9">
+    <p>Created by <a href="https://www.instagram.com/m.rival_kurniawan/" class="text-dark fw-bold">M. Rival Kurniawan</a></p>
+  </footer>
+  <!-- Akhir Footer -->
+</template>
+
+<script>
+export default {};
+</script>
